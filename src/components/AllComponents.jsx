37920e1e1c49b4c7041c101d04caf7e1aeda1f07
MohammadWazahat <muhammadwazahatrza092@gmail.com>
2024-06-19 13:49:23 +0530

    Footer added in all components

--- src/components/AllComponents.jsx
+++ src/components/AllComponents.jsx
@@ -6,27 +6,58 @@ import Products from "./products/Products";
 import SingleProduct from "./singleProduct/SingleProduct";
 import About from "./about/About";
 import Navbar from "./header/Navbar";
+import Footer from "./footer/Footer";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <><Navbar/><Home /></>,
+    element: (
+      <>
+        <Navbar />
+        <Home />
+        <Footer />
+      </>
+    ),
   },
   {
     path: "/about",
-    element: <><Navbar/><About /></>,
+    element: (
+      <>
+        <Navbar />
+        <About />
+        <Footer />
+      </>
+    ),
   },
   {
     path: "/allProducts",
-    element: <><Navbar/><Products /></>,
+    element: (
+      <>
+        <Navbar />
+        <Products />
+        <Footer />
+      </>
+    ),
   },
   {
     path: "/singleproduct/:id",
-    element:<><Navbar/> <SingleProduct /></>,
+    element: (
+      <>
+        <Navbar />
+        <SingleProduct />
+        <Footer />
+      </>
+    ),
   },
   {
     path: "/cart",
-    element: <><Navbar/><Cart /></>,
+    element: (
+      <>
+        <Navbar />
+        <Cart />
+        <Footer />
+      </>
+    ),
   },
 ]);
 
@@ -34,7 +65,6 @@ const AllComponents = () => {
   return (
     <div>
       <RouterProvider router={router}></RouterProvider>
-      
     </div>
   );
 };
